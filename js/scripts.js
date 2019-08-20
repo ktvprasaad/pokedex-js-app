7155392ed748b11ce3b234854dc49380a7f50f03
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2019-08-20 19:04:43 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+alert('Hello World!!'); // displays on a popup window
+
+var great='Hi There!\n';
+document.write(great);
+
+var myName = 'It\'s Vijay';
+/* to write on your DOM instead of displaying on a separate popup window */
+document.write(myName);
+
+myName = ' Prasaad.\n';
+document.write(myName);
+
+var favoriteFood_ = 'Rice Noodles with coconut milk and '
+document.write('My favorite food is ', favoriteFood_);
+
+var math100 = 100;
+document.write('I love doing Maths ',math100,'%!!');
