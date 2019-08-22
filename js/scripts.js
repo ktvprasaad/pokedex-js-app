0a2c9dd88aa7f82f25885367b4e37cc5a7379579
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2019-08-22 08:19:06 -0400

    Comma update
    Updated the array values separated with commas.

--- js/scripts.js
+++ js/scripts.js
@@ -5,28 +5,28 @@ var repository = [
     weight: 6.9,
     types: ['grass','poison'],
     abilities: ['Cholorphyll','Overgrow']
-  }
+  },
   {
     name: 'Charmander',
     height: 6,
     weight: 8.5,
     types: ['fire'],
     abilities: ['Blaze','Solar-power']
-  }
+  },
   {
     name: 'Squirtle',
     height: 5,
     weight: 9,
     types: ['water'],
     abilities: ['Rain-dish','Torrent']
-  }
+  },
   {
     name: 'Spearow',
     height: 3,
     weight: 2,
     types: ['flying','normal'],
     abilities: ['Keen-eye','Sniper']
-  }
+  },
   {
     name: 'Pikachu',
     height: 4,
