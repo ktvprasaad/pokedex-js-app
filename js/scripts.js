f6cc03e730bf57ee87cc7b20e6436ba654249006
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2019-08-27 01:03:31 -0400

    Pokeman and logic to display all
    Updated the script to include one more name and its property in the repository. Also added a logic to display all the names  and their height of the pokeman from the repository thru for loop. Exclaim the big pokeman based on the height and weight condition.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,53 @@
 var repository = [
   {
     name: 'Bulbasaur',
-    height: 7,
+    height: 0.7,
     weight: 6.9,
     types: ['grass','poison'],
     abilities: ['Cholorphyll','Overgrow']
   },
   {
     name: 'Charmander',
-    height: 6,
+    height: 0.6,
     weight: 8.5,
     types: ['fire'],
     abilities: ['Blaze','Solar-power']
   },
   {
     name: 'Squirtle',
-    height: 5,
+    height: 0.5,
     weight: 9,
     types: ['water'],
     abilities: ['Rain-dish','Torrent']
   },
   {
     name: 'Spearow',
-    height: 3,
+    height: 0.3,
     weight: 2,
     types: ['flying','normal'],
     abilities: ['Keen-eye','Sniper']
   },
   {
+    name: 'Charizard',
+    height: 1.7,
+    weight: 90.5,
+    types: ['Fire','Flying'],
+    abilities: ['Blaze','Solar-power']
+  },
+  {
     name: 'Pikachu',
-    height: 4,
+    height: 0.4,
     weight: 6,
     types: ['electric'],
     abilities: ['Static','Lightningrod']
   }
 ];
+
+for (var i = 0; i < repository.length; i++) {
+  document.write(repository[i].name + ' \(height: ' + repository[i].height + '\)\n');
+  var pokeHeight = repository[i].height;
+  var pokeWeight = repository[i].weight;
+  if (pokeHeight > 1 && pokeWeight > 10) {
+    document.write(' - Wow that\'s really a big one!\n');
+  }
+}
