2be5dcfc2c17e5a3be17d1cc9be83881f0018fcd
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2019-09-10 14:11:35 -0400

    New functions in IIFE & event listener changes
    Updated to include new function under IIFE to add new list and button thru create element document function and show the details of the pokemon object on the console thru event listener.

--- js/scripts.js
+++ js/scripts.js
@@ -7,36 +7,36 @@ var pokemonRepository = (function () {
       weight: 6.9,
       types: ['grass','poison'],
       abilities: ['Cholorphyll','Overgrow']
-   },
-   {
+    },
+    {
       name: 'Charmander',
       height: 0.6,
       weight: 8.5,
       types: ['fire'],
       abilities: ['Blaze','Solar-power']
-  },
-  {
+    },
+    {
       name: 'Squirtle',
       height: 0.5,
       weight: 9,
       types: ['water'],
       abilities: ['Rain-dish','Torrent']
-  },
-  {
+    },
+    {
       name: 'Spearow',
       height: 0.3,
       weight: 2,
       types: ['flying','normal'],
       abilities: ['Keen-eye','Sniper']
-  },
-  {
+    },
+    {
       name: 'Charizard',
       height: 1.7,
       weight: 90.5,
       types: ['fire','flying'],
       abilities: ['Blaze','Solar-power']
-  }
-];
+    }
+  ];
 
   function add(pokemon) {
     repository.push(pokemon);
@@ -46,22 +46,51 @@ var pokemonRepository = (function () {
     return repository;
   }
 
+  function addListItem(pokemon) {
+    /* To create 'li' element */
+    var listItemElement = document.createElement('li');
+    /* To create 'button' element */
+    var buttonElement = document.createElement('button');
+    /* To add innertext to button element */
+    buttonElement.innerText = pokemon;
+    /* To append button element to list */
+    listItemElement.appendChild(buttonElement);
+    buttonElement.classList.add('newButton');
+    /* To append 'button' element as a child to the 'li'st element */
+    //listItemElement.appendChild(buttonElement);
+    /* To append 'li'st element as a child 'ul' element */
+    $ul.appendChild(listItemElement);
+    buttonElement.addEventListener('click', function(event){
+      showDetails(event,pokemon);
+    });
+  }
+
+  function showDetails(event, pokemon) {
+    event.target.classList.toggle('button');
+    event.target.classList.toggle('newButton');
+    if (event.target.classList.contains('newButton')) {
+      event.target.innerText = pokemon;
+    } else {
+      console.log(pokemon + ' says hi there!');
+      event.target.innerText = 'You Got It!';
+    }
+  }
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
-//to read all the objects of the repository array
-//console.log(pokemonRepository.getAll());
-
 //new pokemon declaration
 var newPokemon = {
-      name: 'Pikachu',
-      height: 1.4,
-      weight: 6,
-      types: ['electric'],
-      abilities: ['Static','Lightningrod']
+  name: 'Pikachu',
+  height: 1.4,
+  weight: 6,
+  types: ['electric'],
+  abilities: ['Static','Lightningrod']
 };
 
 //document.write(Object.keys(newPokemon));
@@ -78,16 +107,13 @@ if (typeof newPokemon === 'object') {
   alert('Not an object!!');
 }
 
-document.write('<h2>Pokemon Repository thru IIFE</h2>');
+var $ul = document.querySelector('ul');
 
 //property refers to pokemon object's key and pokemon[property] refers to it's value
 pokemonRepository.getAll().forEach(function(pokemon) {
   Object.keys(pokemon).forEach(function(property, index) {
     if (index === 0) {
-      document.write(`<strong>${property}: ${pokemon[property]}</strong><br>`);
-    } else {
-      document.write(`${property}: ${pokemon[property]}<br>`);
+      pokemonRepository.addListItem(pokemon[property]);
     }
   });
-  document.write('--------------------------------<br>');
 });
